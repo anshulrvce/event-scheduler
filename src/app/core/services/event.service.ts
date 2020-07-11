4e643ff2bf0500501ca7948a83c1372a0a30985a
Anshul Bansal <ansbansa@wkmgb4508794.broadband>
2020-07-11 21:37:05 +0100

    added scheduler and event components

---
+++ src/app/core/services/event.service.ts
@@ -0,0 +1,129 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventService {
+
+  processEvents(events: Array<any>): Array<any>  {
+    events = events.sort( (e1, e2) => {
+      if ( e1.start < e2.start ) { return -1; }
+      if (e1.start > e2.start) { return 1; }
+      if (e1.end < e2.end) { return -1; }
+      if (e1.end > e2.end) { return 1; }
+      return 0;
+    });
+    for ( let i = 0; i < events.length; i++) {
+      events[i].count = 1;
+      let endTime = null;
+      events[i].id = i;
+      events[i].collidingEvents = [];
+      for (let j = 0; j < events.length; j++){
+          if ( events[i].start < events[j].start &&  events[j].start < events[i].end ||
+             events[i].start < events[j].end &&  events[j].end < events[i].end
+            || events[i].start === events[j].start &&  events[j].end === events[i].end
+            || events[j].start < events[i].start &&  events[i].start < events[j].end
+            || events[j].start < events[i].end &&  events[i].end < events[j].end  ){
+                if ((events[j].start < endTime || endTime === null) && i !== j ){
+                    endTime = events[j].end;
+                    events[i].count = events[i].count + 1;
+              }
+                events[i].collidingEvents.push(j);
+
+          }
+      }
+    }
+
+    console.log(events);
+    // tslint:disable-next-line: prefer-for-of
+    for ( let i = 0; i < events.length; i++ ){
+        let count = events[i].count;
+
+        for ( let j = 0; j < events[i].collidingEvents.length; j++ ){
+            if (events[events[i].collidingEvents[j]].count > count){
+                count = events[events[i].collidingEvents[j]].count;
+            }
+        }
+        events[i].count = count;
+    }
+
+    for (let i = 0; i < events.length; i++ ){
+      console.log('event numebr', i);
+      let position = 1;
+      let flag = true;
+      const event = events[i];
+      for (let j = 1; j <= event.count; j++ ){
+
+        for (let k = 0; k < event.collidingEvents.length; k++){
+          console.log(events[event.collidingEvents[k]], 'events---------', events[event.collidingEvents[k]].position, j);
+          if (events[event.collidingEvents[k]].position && events[event.collidingEvents[k]].position === j){
+          flag = true;
+          break;
+        } else{
+          console.log('here--falg');
+          flag = false;
+        }
+      }
+        if (flag === false){
+        console.log('break ', j);
+        position = j;
+        break;
+      }
+      }
+
+      events[i].position = position;
+    }
+    console.log('events--------', JSON.stringify(events));
+    return events;
+  }
+  getEvents(): Array<any> {
+    return [
+      {
+        title: 'Greeting Meeting',
+        subtitle: 'Meeting Room A',
+        start: 30,
+        end: 60,
+        id: 1,
+        count: 1,
+        collidingEvents: [],
+      },
+      {
+        title: 'Design Meet',
+        subtitle: 'Meeting Room C',
+        start: 60,
+        end: 120,
+        id: 2,
+        count: 1,
+        collidingEvents: [],
+      },
+      {
+        title: 'Development Meet',
+        subtitle: 'Meeting Room D',
+        start: 300,
+        end: 360,
+        id: 3,
+        count: 1,
+        collidingEvents: [],
+      },
+      {
+        title: 'Architecture Meet',
+        subtitle: 'Meeting Room A',
+        start: 90,
+        end: 300,
+        id: 4,
+        count: 1,
+        collidingEvents: [],
+      },
+      {
+        title: 'Product Meet',
+        subtitle: 'Meeting Room C',
+        start: 120,
+        end: 180,
+        id: 5,
+        count: 1,
+        collidingEvents: [],
+      }
+    ]; // method returns an array of cities
+  }
+}
---
+++ src/app/scheduler/scheduler.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+import { EventService } from '../core/services/event.service';
+
+@Component({
+ selector: 'app-scheduler',
+ templateUrl: './scheduler.component.html',
+ styleUrls: ['./scheduler.component.scss']
+})
+export class SchedulerComponent implements OnInit {
+
+events: Array<any> = [];
+ timeArray: Array<string> = [];
+ chosenCity; // create variable for saving current (chosen) city
+ @HostListener('document:listenToLayout', ['$event'])
+ updateLayout(event) {
+   console.log('Do stuff here');
+ }
+
+
+ constructor(private eventService: EventService) { }
+
+ layOutDay(eventsData){
+  this.events =  this.eventService.processEvents(eventsData);
+  const customEvent = new CustomEvent('listenToLayout');
+  document.dispatchEvent(customEvent);
+}
+
+
+ // tslint:disable-next-line: typedef
+ ngOnInit() {
+   const eventProcessor = this.layOutDay.bind(this);
+   window['layOutDay'] = eventProcessor;
+   eventProcessor(this.eventService.getEvents());
+   this.timeArray = ['9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '1:00',
+    '1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30', '6:00'];
+ }
+}
+

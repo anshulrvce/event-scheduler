2d5a927810d0b0bd791279e3a04f7b7c40f14dff
Anshul Bansal <ansbansa@wkmgb4508794.broadband>
2020-07-12 11:21:06 +0100

    added unit test cases

---
+++ src/app/core/mocks/result.mock.ts
@@ -0,0 +1,101 @@
+export const EXPECTED =
+{
+    processedEvents: [
+        {
+          start: 30,
+          end: 60,
+          count: 1,
+          id: 0,
+          collidingEvents: [ 0 ],
+          position: 1
+        },
+        {
+          start: 60,
+          end: 120,
+          count: 2,
+          id: 1,
+          collidingEvents: [ 1, 2 ],
+          position: 1
+        },
+        {
+          start: 90,
+          end: 300,
+          count: 2,
+          id: 2,
+          collidingEvents: [ 1, 2 ],
+          position: 2
+        },
+        {
+          start: 300,
+          end: 360,
+          count: 1,
+          id: 3,
+          collidingEvents: [ 3 ],
+          position: 1
+        }
+      ],
+    positionedEvents: [
+    {
+      start: 30,
+      end: 60,
+      id: 0,
+      count: 1,
+      collidingEvents: [ 0 ],
+      position: 1
+    },
+    {
+      start: 60,
+      end: 120,
+      id: 1,
+      count: 2,
+      collidingEvents: [ 1, 2 ],
+      position: 1
+    },
+    {
+      start: 90,
+      end: 300,
+      id: 2,
+      count: 2,
+      collidingEvents: [ 1, 2 ],
+      position: 2
+    },
+    {
+      start: 300,
+      end: 360,
+      id: 3,
+      count: 1,
+      collidingEvents: [ 3 ],
+      position: 1
+    }
+  ],
+  colSpanEvents: [
+    {
+      start: 30,
+      end: 60,
+      id: 0,
+      count: 1,
+      collidingEvents: [ 0 ]
+    },
+    {
+      start: 60,
+      end: 120,
+      id: 1,
+      count: 2,
+      collidingEvents: [ 1, 2 ]
+    },
+    {
+      start: 90,
+      end: 300,
+      id: 2,
+      count: 2,
+      collidingEvents: [ 1, 2 ]
+    },
+    {
+      start: 300,
+      end: 360,
+      id: 3,
+      count: 1,
+      collidingEvents: [ 3 ]
+    }
+  ]
+};
--- src/app/core/services/event.service.spec.ts
+++ src/app/core/services/event.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EventService } from './event.service';
+import { MOCKS } from '../mocks/events.mock';
+import {EXPECTED} from '../mocks/result.mock';
 
 describe('EventService', () => {
   let service: EventService;
@@ -12,5 +14,17 @@ describe('EventService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('positonEvents should return expected value', () => {
+    expect(service.positonEvents(MOCKS.eventPositionMock)).toEqual(EXPECTED.positionedEvents);
+  });
+
+  it('calculateColSpanEvents should return expected value', () => {
+    expect(service.calculateColSpanEvents(MOCKS.colSpanEvents)).toEqual(EXPECTED.colSpanEvents);
+  });
+
+  it('processEvents should return expected value', () => {
+    expect(service.processEvents(MOCKS.eventMock)).toEqual(EXPECTED.processedEvents);
   });
 });

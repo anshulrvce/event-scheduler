c377b02cde86fc4785f36dedd8129a3d7fd164ce
Anshul Bansal <ansbansa@wkmgb4508794.broadband>
2020-07-12 10:49:56 +0100

    handled extra scenarios

--- src/app/core/services/event.service.ts
+++ src/app/core/services/event.service.ts
@@ -7,71 +7,92 @@ import { Event } from '../interfaces/event';
 })
 export class EventService {
 
+  calculateColSpanEvents(events: Array<any>): Array<any> {
+    // tslint:disable-next-line: prefer-for-of
+    for ( let i = 0; i < events.length; i++ ){
+      let count = events[i].count;
+      // tslint:disable-next-line: prefer-for-of
+      for ( let j = 0; j < events[i].collidingEvents.length; j++ ){
+          if (events[events[i].collidingEvents[j]].count > count){
+              count = events[events[i].collidingEvents[j]].count;
+          }
+      }
+      events[i].count = count;
+  }
+    return events;
+  }
+
+  positonEvents(events: Array<any>): Array<any>{
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < events.length; i++ ){
+      let position = 1;
+      let flag = true;
+      const event = events[i];
+      for (let j = 1; j <= event.count; j++ ){
+        // tslint:disable-next-line: prefer-for-of
+        for (let k = 0; k < event.collidingEvents.length; k++){
+        if (events[event.collidingEvents[k]].position && events[event.collidingEvents[k]].position === j){
+          flag = true;
+          break;
+        } else{
+          flag = false;
+        }
+      }
+        if (flag === false){
+          position = j;
+          break;
+        }
+      }
+      events[i].position = position;
+    }
+    return events;
+  }
+
   processEvents(events: Array<any>): Array<any>  {
-    events = events.sort( (e1, e2) => {
+    events =   events = events.sort( (e1, e2) => {
       if ( e1.start < e2.start ) { return -1; }
       if (e1.start > e2.start) { return 1; }
       if (e1.end < e2.end) { return -1; }
       if (e1.end > e2.end) { return 1; }
       return 0;
     });
+
     for ( let i = 0; i < events.length; i++) {
       events[i].count = 1;
       let endTime = null;
+      let flag;
+      const endArray = [];
       events[i].id = i;
       events[i].collidingEvents = [];
       for (let j = 0; j < events.length; j++){
-          if ( events[i].end > events[j].start && events[i].start < events[j].end ){
+          flag = false;
+          if ( events[i].end > events[j].start && events[i].start < events[j].end  ){
                 if ((events[j].start < endTime || endTime === null) && i !== j ){
-                    endTime = events[j].end;
-                    events[i].count = events[i].count + 1;
+                  endArray.push(events[j].end);
+                  console.log(endArray);
+                  for (let k = 0; k < endArray.length; k++){
+                      if (events[j].start >= endArray[k]){
+                        endArray[k] = events[j].end;
+                        flag = true;
+                      }
+                    }
+                  if (flag === false){
+                      events[i].count = events[i].count + 1;
+                    }
+              }
+                if (i !== j) {
+              endTime = events[j].end;
               }
                 events[i].collidingEvents.push(j);
 
           }
       }
     }
-    for (let i = 0; i < events.length; i++ ){
-      console.log('event numebr', i);
-      let position = 1;
-      let flag = true;
-      const event = events[i];
-      for (let j = 1; j <= event.count; j++ ){
-
-        for (let k = 0; k < event.collidingEvents.length; k++){
-          console.log(events[event.collidingEvents[k]], 'events---------', events[event.collidingEvents[k]].position, j);
-          if (events[event.collidingEvents[k]].position && events[event.collidingEvents[k]].position === j){
-          flag = true;
-          break;
-        } else{
-          console.log('here--falg');
-          flag = false;
-        }
-      }
-        if (flag === false){
-        console.log('break ', j);
-        position = j;
-        break;
-      }
-      }
-
-      events[i].position = position;
-    }
-
-    console.log(JSON.stringify(events));
     // tslint:disable-next-line: prefer-for-of
-    for ( let i = 0; i < events.length; i++ ){
-        let count = events[i].count;
+    events = this.calculateColSpanEvents(events);
+    events = this.positonEvents(events);
 
-        for ( let j = 0; j < events[i].collidingEvents.length; j++ ){
-            if (events[events[i].collidingEvents[j]].count > count){
-                count = events[events[i].collidingEvents[j]].count;
-            }
-        }
-        events[i].count = count;
-    }
-
-    console.log('events--------', JSON.stringify(events));
+    console.log(events)
     return events;
   }
   getEvents(): Array<Event> {
@@ -108,6 +129,24 @@ export class EventService {
         subtitle: 'Meeting Room A',
         start: 90,
         end: 300,
+        id: 4,
+        count: 1,
+        collidingEvents: [],
+      },
+      {
+        title: 'Architecture Meet',
+        subtitle: 'Meeting Room A',
+        start: 20,
+        end: 300,
+        id: 4,
+        count: 1,
+        collidingEvents: [],
+      },
+      {
+        title: 'Architecture Meet',
+        subtitle: 'Meeting Room A',
+        start: 220,
+        end: 389,
         id: 4,
         count: 1,
         collidingEvents: [],
